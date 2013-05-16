e7ed75e2c52ecacf66e089448457904d6a0f68a1
louis gueye <louis.gueye@figarocms.fr>
2013-05-16 18:40:39 +0200

    Initial commit

---
+++ src/main/scala/org/diveintojee/ninetynineproblems/P01.scala
@@ -0,0 +1,14 @@
+// P01 (*) Find the last element of a list.
+//     Example:
+//     scala> last(List(1, 1, 2, 3, 5, 8))
+//     res0: Int = 8
+
+// The start of the definition of last should be
+//     def last[A](l: List[A]): A = ...
+// The `[A]` allows us to handle lists of any type.
+
+object P01 extends App {
+
+  def last[A] (list: List[A]): A = ???
+
+}
---
+++ src/test/scala/org/diveintojee/ninetynineproblems/P01Suite.java
@@ -0,0 +1,14 @@
+package org.diveintojee.ninetynineproblems;
+
+import org.junit.runner.RunWith;
+import org.scalatest.FunSuite;
+import org.scalatest.junit.JUnitRunner;
+
+@RunWith(classOf[JUnitRunner])
+public class P01Suite extends FunSuite {
+
+  @Test
+  public void testLast() throws Exception {
+
+  }
+}
