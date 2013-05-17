c8edbffc4ef9e5e85cb3bcb8fc9e8e0a261ac09a
louis gueye <louis.gueye@figarocms.fr>
2013-05-17 11:07:09 +0200

    solve P02

---
+++ src/main/scala/org/diveintojee/ninetynineproblems/P02.scala
@@ -0,0 +1,13 @@
+package org.diveintojee.ninetynineproblems
+
+import java.util.NoSuchElementException
+
+object P02 extends App {
+
+  def penultimate[A] (list: List[A]): A = list match {
+    case head :: _ :: Nil  => head
+    case head :: tail  => penultimate(tail)
+    case _          => throw new NoSuchElementException
+  }
+
+}
---
+++ src/test/scala/org/diveintojee/ninetynineproblems/P02Suite.scala
@@ -0,0 +1,17 @@
+package org.diveintojee.ninetynineproblems
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+import org.diveintojee.ninetynineproblems.P02._
+
+@RunWith(classOf[JUnitRunner])
+class P02Suite extends FunSuite {
+
+  test("penultimate should succeed") {
+    val actual: Int = penultimate(List(1, 1, 2, 3, 5, 8))
+    val expected: Int = 5
+    assert(actual === expected)
+  }
+}
