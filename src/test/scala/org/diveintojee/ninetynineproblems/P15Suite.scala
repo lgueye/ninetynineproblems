75ed17852b10dc41f45294abaff634ef0eb88644
louis gueye <louis.gueye@figarocms.fr>
2013-05-21 18:37:38 +0200

    solve P16

--- src/test/scala/org/diveintojee/ninetynineproblems/P15Suite.scala
+++ src/test/scala/org/diveintojee/ninetynineproblems/P15Suite.scala
@@ -10,7 +10,7 @@ import org.scalatest.matchers.ShouldMatchers
 @RunWith(classOf[JUnitRunner])
 class P15Suite extends FunSuite with ShouldMatchers {
 
-  test("duplicate should succeed") {
+  test("duplicateN should succeed") {
     val actual: List[Symbol] = duplicateN(3, List('a, 'b, 'c, 'c, 'd))
     val expected: List[Symbol] = List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
     assert(actual === expected)
