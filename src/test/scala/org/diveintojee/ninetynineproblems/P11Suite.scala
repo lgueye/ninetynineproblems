be1c219ecd59021e789297f89a020e0cfb7fe509
louis gueye <louis.gueye@figarocms.fr>
2013-05-21 13:51:25 +0200

    P11Suite typo

--- src/test/scala/org/diveintojee/ninetynineproblems/P11Suite.scala
+++ src/test/scala/org/diveintojee/ninetynineproblems/P11Suite.scala
@@ -10,7 +10,7 @@ import org.scalatest.matchers.ShouldMatchers
 @RunWith(classOf[JUnitRunner])
 class P11Suite extends FunSuite with ShouldMatchers {
 
-  test("pack should succeed") {
+  test("encodeModified should succeed") {
     val actual: List[Any] = encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
     val expected: List[Any] = List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
     assert(actual === expected)
