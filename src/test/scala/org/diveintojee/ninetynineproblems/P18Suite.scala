692cc8aef495ffa3ddc804a8684b1dd168357049
louis gueye <louis.gueye@figarocms.fr>
2013-05-22 16:26:54 +0200

    add more tests for P18

--- src/test/scala/org/diveintojee/ninetynineproblems/P18Suite.scala
+++ src/test/scala/org/diveintojee/ninetynineproblems/P18Suite.scala
@@ -10,6 +10,16 @@ import org.scalatest.matchers.ShouldMatchers
 @RunWith(classOf[JUnitRunner])
 class P18Suite extends FunSuite with ShouldMatchers {
 
+  test("slice should produce NoSuchElementException with illegal bounds") {
+    evaluating { slice(4, 3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) } should produce [IllegalArgumentException]
+  }
+
+  test("slice should produce Nil with equal bounds") {
+    val actual: List[Any] = slice(3, 3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+    val expected: List[Any] = Nil
+    assert(actual === expected)
+  }
+
   test("slice should succeed") {
     val actual: List[Any] = slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
     val expected: List[Any] = List('d, 'e, 'f, 'g)
