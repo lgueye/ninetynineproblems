11f32ea3ff145e483b3d005ade0fda8a1339cae7
louis gueye <louis.gueye@figarocms.fr>
2013-05-27 19:12:49 +0200

    Create tests

--- src/main/scala/org/diveintojee/ninetynineproblems/P27.scala
+++ src/main/scala/org/diveintojee/ninetynineproblems/P27.scala
@@ -4,10 +4,32 @@ import P26.combinations
 
 object P27 extends App {
 
+  /**
+   *
+   * @param list
+   * @tparam A
+   * @return
+   */
   def group3[A](list: List[A]): List[List[List[A]]] = for {
-    a <- combinations(2, list)
-    exceptA = list.filter(x => x != a)
-    b <- combinations(2, exceptA)
-  } yield List(a, b, exceptA.filter(y => y != b))
+    firstCombination <- combinations(2, list)
+    exceptFirst = list diff firstCombination
+    secondCombination <- combinations(3, exceptFirst)
+    exceptSecond = exceptFirst diff secondCombination
+  } yield List(firstCombination, secondCombination, exceptSecond)
+
+  /**
+   *
+   * @param distribution
+   * @param list
+   * @tparam A
+   * @return
+   */
+  def group[A](distribution: List[Int], list: List[A]): List[List[List[A]]] = distribution match {
+    case Nil          => List(Nil)
+    case head :: tail =>
+      combinations(head, list) flatMap {
+        combination => group(tail, list diff combination) map { combination :: _}
+      }
+  }
 
 }
--- src/test/scala/org/diveintojee/ninetynineproblems/P27Suite.scala
+++ src/test/scala/org/diveintojee/ninetynineproblems/P27Suite.scala
@@ -6,19 +6,31 @@ import org.scalatest.junit.JUnitRunner
 
 import org.diveintojee.ninetynineproblems.P27._
 import org.scalatest.matchers.ShouldMatchers
+import scala.io.Source
 
 @RunWith(classOf[JUnitRunner])
 class P27Suite extends FunSuite with ShouldMatchers {
 
+  def loadExpected(s: String): List[List[List[String]]] = {
+    //val lines: List[String]  = io.Source.fromInputStream(getClass.getResourceAsStream(s)).getLines().toList
+    val lines: List[String]  = Source.fromURL(getClass.getResource(s)).getLines().toList
+    val tmp: List[List[String]] = lines map { _.split("|").toList }
+    println(tmp)
+    Nil
+  }
+
+  test("group should succeed") {
+    val expected = loadExpected("/expected-group.txt")
+    println(expected)
+//    val actual = group(List(2, 2, 5), List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
+//    assert(actual === expected)
+  }
+
   test("group3 should succeed") {
-    val expected: List[Any] = List(
-      List(List("Aldo"), List("Beat", "Carla")),
-      List(List("Beat"), List("Carla", "Aldo")),
-      List(List("Carla"), List("Aldo", "Beat"))
-    )
-    //val actual: List[Any] = group3(List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
-    val actual: List[Any] = group3(List("Aldo", "Beat", "Carla"))
-    assert(actual === expected)
+    val expected = loadExpected("/expected-group3.txt")
+    println(expected)
+//    val actual = group3(List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary","Hugo", "Ida"))
+//    assert(actual === expected)
   }
 
 }
