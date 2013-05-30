a0fc243afac4ac7e648117b4406bd15c0bd14d63
louis gueye <louis.gueye@figarocms.fr>
2013-05-30 11:09:17 +0200

    solve P28

--- src/main/scala/org/diveintojee/ninetynineproblems/P28.scala
+++ src/main/scala/org/diveintojee/ninetynineproblems/P28.scala
@@ -1,17 +1,14 @@
 package org.diveintojee.ninetynineproblems
 
+import P10.encode
+
 object P28 extends App {
 
   def lsort[A](list: List[List[A]]): List[List[A]] = list.sortBy(_.size)
 
-  private def lsortFreqAcc[A](frequencies: List[Int], frequencyMap: Map[Int, List[List[A]]], acc: List[List[A]]): List[List[A]] = frequencies match {
-    case Nil          => acc
-    case head :: tail => lsortFreqAcc(tail, frequencyMap, frequencyMap(head) ++ acc)
-  }
-
   def lsortFreq[A](list: List[List[A]]): List[List[A]] = {
-    val frequencyMap = list.groupBy(_.size)
-    lsortFreqAcc(frequencyMap.keySet.toList.sorted, frequencyMap, List(Nil))
+    val lengthFrequencies = Map( encode(list map {_.length} sortWith { (x,y) => x < y}) map {_.swap} : _*)
+    list sortWith  { (e1, e2) => lengthFrequencies(e1.length) < lengthFrequencies(e2.length) }
   }
 
 }
