a4d1917a0c9aea7bf352cef30194fec14abe69d3
louis gueye <louis.gueye@figarocms.fr>
2013-06-14 13:17:56 +0200

    solve P46,47

--- src/main/scala/org/diveintojee/ninetynineproblems/logic1.scala
+++ src/main/scala/org/diveintojee/ninetynineproblems/logic1.scala
@@ -2,39 +2,51 @@ package org.diveintojee.ninetynineproblems {
 
   import scala._
 
-  object S99Logic {
+  class S99Logic(a: Boolean) {
 
-    def and(a: Boolean, b: Boolean) =
+    import S99Logic._
+
+    def and(b: Boolean): Boolean =
       (a, b) match {
         case (true, true) => true
         case _            => false
       }
+    def or(b: Boolean): Boolean =
+      (a, b) match {
+        case (true, _) => true
+        case (_, true) => true
+        case _         => false
+      }
 
-    def or(a: Boolean, b: Boolean) =
-    (a, b) match {
-      case (true, _) => true
-      case (_, true) => true
-      case _            => false
+    def nor(b: Boolean): Boolean =
+      not(a or b)
+
+    def equ(b: Boolean): Boolean =
+      (a and b) or (not(a) and not(b))
+
+    def xor(b: Boolean): Boolean =
+      not(a equ b)
+
+    def nand(b: Boolean): Boolean =
+      not(a and b)
+
+    def impl(b: Boolean): Boolean =
+      not(a) or b
+  }
+
+  object S99Logic {
+    implicit def boolean2S99Logic(a: Boolean): S99Logic = new S99Logic(a)
+
+    def not(a: Boolean) = a match {
+      case true  => false
+      case false => true
     }
 
-    def nand(a: Boolean, b: Boolean) =
-      and(a,b) match {
-        case true => false
-        case false => true
-      }
+    def table2(f: (Boolean, Boolean) => Boolean) {
+      println("A     B     result")
+      for {a <- List(true, false);b <- List(true, false)} println("%-5s %-5s %-5s\n", a, b, f(a, b))
 
-    def nor(a: Boolean, b: Boolean) =
-      or(a,b) match {
-        case true => false
-        case false => true
-      }
-
-    def xor(a: Boolean, b: Boolean) =
-      (a, b) match {
-        case (true, true) => false
-        case _            => or(a,b)
-      }
-
+    }
   }
 
 }
--- src/test/scala/org/diveintojee/ninetynineproblems/S99LogicSuite.scala
+++ src/test/scala/org/diveintojee/ninetynineproblems/S99LogicSuite.scala
@@ -7,126 +7,177 @@ import org.scalatest.junit.JUnitRunner
 import org.diveintojee.ninetynineproblems.S99Logic._
 import org.scalatest.matchers.ShouldMatchers
 
+/**
+ * Truth table tests are base on material from <a href="http://en.wikipedia.org/wiki/Truth_table">Wikipedia truth tables</a>
+ */
 @RunWith(classOf[JUnitRunner])
 class S99LogicSuite extends FunSuite with ShouldMatchers {
 
   test("and(true, true) should succeed") {
-    val actual = and(true, true)
+    val actual = true.and(true)
     val expected = true
     assert(actual === expected)
   }
 
-  test("and(false, true) should succeed") {
-    val actual = and(false, true)
+  test("and(true, false) should succeed") {
+    val actual = true.and(false)
     val expected = false
     assert(actual === expected)
   }
 
-  test("and(true, false) should succeed") {
-    val actual = and(true, false)
+  test("and(false, true) should succeed") {
+    val actual = false.and(true)
     val expected = false
     assert(actual === expected)
   }
 
   test("and(false, false) should succeed") {
-    val actual = and(false, false)
+    val actual = false.and(false)
     val expected = false
     assert(actual === expected)
   }
 
   test("or(true, true) should succeed") {
-    val actual = or(true, true)
-    val expected = true
-    assert(actual === expected)
-  }
-
-  test("or(false, true) should succeed") {
-    val actual = or(false, true)
+    val actual = true.or(true)
     val expected = true
     assert(actual === expected)
   }
 
   test("or(true, false) should succeed") {
-    val actual = or(true, false)
+    val actual = true.or(false)
+    val expected = true
+    assert(actual === expected)
+  }
+
+  test("or(false, true) should succeed") {
+    val actual = false.or(true)
     val expected = true
     assert(actual === expected)
   }
 
   test("or(false, false) should succeed") {
-    val actual = or(false, false)
+    val actual = false.or(false)
     val expected = false
     assert(actual === expected)
   }
 
   test("nand(true, true) should succeed") {
-    val actual = nand(true, true)
+    val actual = true.nand(true)
     val expected = false
     assert(actual === expected)
   }
 
-  test("nand(false, true) should succeed") {
-    val actual = nand(false, true)
+  test("nand(true, false) should succeed") {
+    val actual = true.nand(false)
     val expected = true
     assert(actual === expected)
   }
 
-  test("nand(true, false) should succeed") {
-    val actual = nand(true, false)
+  test("nand(false, true) should succeed") {
+    val actual = false.nand(true)
     val expected = true
     assert(actual === expected)
   }
 
   test("nand(false, false) should succeed") {
-    val actual = nand(false, false)
+    val actual = false.nand(false)
     val expected = true
     assert(actual === expected)
   }
 
   test("nor(true, true) should succeed") {
-    val actual = nor(true, true)
-    val expected = false
-    assert(actual === expected)
-  }
-
-  test("nor(false, true) should succeed") {
-    val actual = nor(false, true)
+    val actual = true.nor(true)
     val expected = false
     assert(actual === expected)
   }
 
   test("nor(true, false) should succeed") {
-    val actual = nor(true, false)
+    val actual = true.nor(false)
+    val expected = false
+    assert(actual === expected)
+    }
+
+  test("nor(false, true) should succeed") {
+    val actual = false.nor(true)
     val expected = false
     assert(actual === expected)
   }
 
   test("nor(false, false) should succeed") {
-    val actual = nor(false, false)
+    val actual = false.nor(false)
     val expected = true
     assert(actual === expected)
   }
 
   test("xor(true, true) should succeed") {
-    val actual = xor(true, true)
+    val actual = true.xor(true)
     val expected = false
     assert(actual === expected)
   }
 
-  test("xor(false, true) should succeed") {
-    val actual = xor(false, true)
+  test("xor(true, false) should succeed") {
+    val actual = true.xor(false)
     val expected = true
     assert(actual === expected)
   }
 
-  test("xor(true, false) should succeed") {
-    val actual = xor(true, false)
+  test("xor(false, true) should succeed") {
+    val actual = false.xor(true)
     val expected = true
     assert(actual === expected)
   }
 
   test("xor(false, false) should succeed") {
-    val actual = xor(false, false)
+    val actual = false.xor(false)
     val expected = false
+    assert(actual === expected)
+  }
+
+  test("impl(true, true) should succeed") {
+    val actual = true.impl(true)
+    val expected = true
+    assert(actual === expected)
+  }
+
+  test("impl(true, false) should succeed") {
+    val actual = true.impl(false)
+    val expected = false
+    assert(actual === expected)
+  }
+
+  test("impl(false, true) should succeed") {
+    val actual = false.impl(true)
+    val expected = true
+    assert(actual === expected)
+  }
+
+  test("impl(false, false) should succeed") {
+    val actual = false.impl(false)
+    val expected = true
+    assert(actual === expected)
+  }
+
+  test("equ(true, true) should succeed") {
+    val actual = true.equ(true)
+    val expected = true
+    assert(actual === expected)
+  }
+
+  test("equ(true, false) should succeed") {
+    val actual = true.equ(false)
+    val expected = false
+    assert(actual === expected)
+  }
+
+  test("equ(false, true) should succeed") {
+    val actual = false.equ(true)
+    val expected = false
+    assert(actual === expected)
+  }
+
+  test("equ(false, false) should succeed") {
+    val actual = false.equ(false)
+    val expected = true
     assert(actual === expected)
   }
 
